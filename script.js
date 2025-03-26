58313b813a09715bedaf4f10e8dcd091a4cdc0bc
kropn <dialitsafe@gmail.com>
2025-03-26 12:22:52 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -11,10 +11,21 @@ document.addEventListener('DOMContentLoaded', function() {
     const hiddenLinks = document.querySelectorAll('.black-text');
     hiddenLinks.forEach(link => {
         link.addEventListener('mouseover', () => {
-            link.style.color = 'green';
+            link.style.color = 'orange';
         });
         link.addEventListener('mouseout', () => {
             link.style.color = 'black';
         });
     });
+
+    const randomFlickerElements = document.querySelectorAll('.black-text, .glitch-text, .ascii-art');
+    setInterval(() => {
+        randomFlickerElements.forEach(element => {
+            if (Math.random() > 0.7) {
+                element.style.color = 'orange';
+            } else {
+                element.style.color = 'black';
+            }
+        });
+    }, 1000);
 });
